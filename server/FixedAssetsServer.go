81ceb78ba97d2292d68c198a5b0705ebae15bb93
Yulian Adolfo Rojas <Yulianrojas2000@gmail.com>
2021-11-04 23:39:57 -0500

    code to get areas depending of campus and get reasons from page

--- server/FixedAssetsServer.go
+++ server/FixedAssetsServer.go
@@ -14,6 +14,12 @@ type places struct {
 	ID    string
 	PLACE string
 }
+type reasons struct {
+	Reason_change string
+}
+type stateSuccess struct {
+	State string
+}
 
 const (
 	username = "assetsUser"
@@ -34,28 +40,28 @@ func connectToData() *sql.DB {
 }
 
 // function to get especialists area campus
-func getEspecialistsAreasCampus() string {
+func getSpecialistsAreasCampus() string {
 	databaseConnection := connectToData()
 	query, err := databaseConnection.Query("SELECT * FROM fa_esp_campus")
 	if err != nil {
-		fmt.Println("Error selecting data - especialist-area-campus: " + err.Error())
+		fmt.Println("Error selecting data - specialist-area-campus: " + err.Error())
 	}
 	var data places
 	var dataArray []string
 	for query.Next() {
 		if err = query.Scan(&data.ID, &data.PLACE); err != nil {
-			fmt.Println("Error scanning the data especialist-area-campus: " + err.Error())
+			fmt.Println("Error scanning the data specialist-area-campus: " + err.Error())
 		}
 		storage := places{ID: data.ID, PLACE: data.PLACE}
 		toJson, err := json.Marshal(storage)
 		if err != nil {
-			fmt.Println("Error marshalling the data in query scan especialist-area-campus: " + err.Error())
+			fmt.Println("Error marshalling the data in query scan specialist-area-campus: " + err.Error())
 		}
 		dataArray = append(dataArray, string(toJson))
 	}
 	dataToJson, err := json.Marshal(dataArray)
 	if err != nil {
-		fmt.Println("Error marshalling the data in especialist-area-campus: " + err.Error())
+		fmt.Println("Error marshalling the data in specialist-area-campus: " + err.Error())
 	}
 	defer databaseConnection.Close()
 	return string(dataToJson)
@@ -89,17 +95,63 @@ func getMainAreasCampus() string {
 	defer databaseConnection.Close()
 	return string(dataToJson)
 }
+func getReasonChange() string {
+	databaseConnection := connectToData()
+	query, err := databaseConnection.Query("SELECT REASON_CHANGE FROM fa_reason_change")
+	if err != nil {
+		fmt.Println("Error in query get-main-areas: " + err.Error())
+	}
+	var data reasons
+	var dataArray []string
+	for query.Next() {
+		err = query.Scan(&data.Reason_change)
+		if err != nil {
+			fmt.Println("Error scanning the data in get-reason: " + err.Error())
+		}
+		storage := reasons{Reason_change: data.Reason_change}
+		toJson, err := json.Marshal(storage)
+		if err != nil {
+			fmt.Println("Error marshalling the reasons " + err.Error())
+		}
+		dataArray = append(dataArray, string(toJson))
+	}
+	dataToJson, err := json.Marshal(dataArray)
+	if err != nil {
+		fmt.Println("Error marshalling the data in dataArray: " + err.Error())
+	}
+	defer databaseConnection.Close()
+	return string(dataToJson)
+}
 func receiveNewRequest(w http.ResponseWriter, r *http.Request) {
-
+	success := stateSuccess{
+		State: "success",
+	}
+	toJson, err := json.Marshal(success)
+	if err != nil {
+		fmt.Println("Error " + err.Error())
+	}
+	fmt.Fprint(w, string(toJson))
 }
 func accountUser(w http.ResponseWriter, r *http.Request) {
 	accountPage := template.Must(template.ParseFiles("../users/app.html"))
 	accountPage.Execute(w, nil)
+}
+func specialistsAreas(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, getSpecialistsAreasCampus())
+}
+func mainAreas(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, getMainAreasCampus())
+}
+func reasonWhyChange(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, getReasonChange())
 }
 func main() {
 	publicSpace := http.FileServer(http.Dir("../public"))
 	http.Handle("/public/", http.StripPrefix("/public/", publicSpace))
 	http.HandleFunc("/new-request-asset", receiveNewRequest)
 	http.HandleFunc("/account-new-request", accountUser)
+	http.HandleFunc("/specialists-areas", specialistsAreas)
+	http.HandleFunc("/main-areas", mainAreas)
+	http.HandleFunc("/reason-change", reasonWhyChange)
 	http.ListenAndServe(":5200", nil)
 }

700f87293001b7d53e39f52ea8f9ffba186b7e0d
Yulian Adolfo Rojas <Yulianrojas2000@gmail.com>
2021-11-09 12:20:04 -0500

    redesigned the window add items panel and code to show alert to users

--- public/javascript.js
+++ public/javascript.js
@@ -9,7 +9,7 @@ let selectReasonChange = document.getElementById("select-reason-asset")
 let addedItemsList = []
 let panel = null
 let modal = null
-const ADDRESS_SERVER = "http://172.16.0.59:5200/"
+const ADDRESS_SERVER = "http://192.168.1.18:5200/"
 addItems.onclick = () => {
     modal.style.display = "block"
 }
@@ -17,23 +17,23 @@ selectCampusAssetEmit.onchange = (e) => {
     dataFromAreasCampus(e.target.value, selectPlaceAssetEmit)
 }
 selectCampusRecep.onchange = (e) => {
-    dataFromAreasCampus(e.target.value, selectAreaRecep)   
+    dataFromAreasCampus(e.target.value, selectAreaRecep)
 }
 async function dataFromAreasCampus(campus, selectElement) {
     console.log(campus)
-    switch(campus) {
+    switch (campus) {
         case "0":
             removeItemsList(selectElement)
             break;
         case "1":
-            retrievingDataProgress() 
+            retrievingDataProgress()
             var content = await getAreas("main-areas")
             removeItemsList(selectElement)
             appendItemsToList(selectElement, content)
             removeLastElementApp()
             break;
         case "2":
-            retrievingDataProgress() 
+            retrievingDataProgress()
             var content = await getAreas("specialists-areas")
             removeItemsList(selectElement)
             appendItemsToList(selectElement, content)
@@ -46,22 +46,22 @@ async function dataFromAreasCampus(campus, selectElement) {
     }
     function getAreas(URL) {
         // requesting the areas dependig of campus
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             fetch(ADDRESS_SERVER + URL, {
-                method:"GET",
+                method: "GET",
                 headers: {
-                    'content-type':'application/json'
+                    'content-type': 'application/json'
                 }
             })
-            .then(response => response.json())
-            .then(data => resolve(data))
-            .catch((error) => reject(error))
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch((error) => reject(error))
         })
     }
 
 }
 function appendItemsToList(selectElement, dataInJson, d) {
-    for(var i =0; i < dataInJson.length; i++) {
+    for (var i = 0; i < dataInJson.length; i++) {
         var data = JSON.parse(dataInJson[i])
         var opt = document.createElement("option")
         opt.innerHTML = data.PLACE
@@ -69,7 +69,7 @@ function appendItemsToList(selectElement, dataInJson, d) {
     }
 }
 function removeItemsList(selectElement) {
-    while(selectElement.children.length > 1) {
+    while (selectElement.children.length > 1) {
         selectElement.removeChild(selectElement.lastChild)
     }
 }
@@ -80,7 +80,6 @@ function create_modal_form(windowDiv) {
     var textboxDescriptionItem = document.createElement("input")
     var buttonAddItem = document.createElement("button")
     var containerTable = document.createElement("div")
-    var buttonClose = document.createElement("div")
     var button = document.createElement("button")
     let tr = document.createElement("tr")
     // creating table
@@ -106,50 +105,52 @@ function create_modal_form(windowDiv) {
     buttonAddItem.innerHTML = "Agregar"
     button.innerHTML = "Aceptar"
     container.classList.add("container-add-items")
-    buttonClose.classList.add("button-div-close")
     containerTable.classList.add("container-table")
     header.appendChild(textBoxNameItem)
     header.appendChild(textboxDescriptionItem)
     header.appendChild(buttonAddItem)
-    container.appendChild(header)
     containerTable.appendChild(tableAddedItem)
     container.appendChild(containerTable)
-    buttonClose.appendChild(button)
-    container.appendChild(buttonClose)
+    header.appendChild(button)
+    container.appendChild(header)
     windowDiv.appendChild(container)
 
     windowDiv.style.display = "none"
     buttonAddItem.onclick = () => {
         if (textBoxNameItem.value != "") {
-            var trContent = document.createElement("tr")
-            var tdName = document.createElement("td")
-            var tdDescription = document.createElement("td")
-            var tdDelete = document.createElement("td")
-            var iconX = document.createElement("i")
+            if (addedItemsList.length < 15) {
+                var trContent = document.createElement("tr")
+                var tdName = document.createElement("td")
+                var tdDescription = document.createElement("td")
+                var tdDelete = document.createElement("td")
+                var iconX = document.createElement("i")
 
-            iconX.classList.add("fas", "fa-times")
-            iconX.style.color = "red"
-            iconX.style.cursor = "pointer"
-            iconX.style.fontSize = "18px"
-            iconX.style.fontWeight = "bold"
-            let nItem = textBoxNameItem.value
-            let dItem = textboxDescriptionItem.value
-            tdDelete.appendChild(iconX)
-            trContent.appendChild(tdName).innerHTML = nItem
-            trContent.appendChild(tdDescription).innerHTML = dItem
-            trContent.appendChild(tdDelete)
-            textBoxNameItem.value = ""
-            textboxDescriptionItem.value = ""
-            tableAddedItem.appendChild(trContent)
-            var appendData = {
-                nameItem: nItem,
-                descriptionItem: dItem
-            }
-            var toJson = JSON.stringify(appendData)
-            addedItemsList.push(toJson)
-            iconX.onclick = (e) => {
-                addedItemsList = addedItemsList.filter(item => item !== toJson)
-                tableAddedItem.removeChild(e.target.parentElement.parentElement)
+                iconX.classList.add("fas", "fa-times")
+                iconX.style.color = "red"
+                iconX.style.cursor = "pointer"
+                iconX.style.fontSize = "18px"
+                iconX.style.fontWeight = "bold"
+                let nItem = textBoxNameItem.value
+                let dItem = textboxDescriptionItem.value
+                tdDelete.appendChild(iconX)
+                trContent.appendChild(tdName).innerHTML = nItem
+                trContent.appendChild(tdDescription).innerHTML = dItem
+                trContent.appendChild(tdDelete)
+                textBoxNameItem.value = ""
+                textboxDescriptionItem.value = ""
+                tableAddedItem.appendChild(trContent)
+                var appendData = {
+                    nameItem: nItem,
+                    descriptionItem: dItem
+                }
+                var toJson = JSON.stringify(appendData)
+                addedItemsList.push(toJson)
+                iconX.onclick = (e) => {
+                    addedItemsList = addedItemsList.filter(item => item !== toJson)
+                    tableAddedItem.removeChild(e.target.parentElement.parentElement)
+                }
+            } else {
+                alert("Ha alcanzado el límte maximo de registros")
             }
         }
     }
@@ -195,26 +196,26 @@ function data_for_request() {
     if (assetName != "" && brand != "" && model != "" && serial != "" && emitionCampus != "Seleccione una opción" &&
         emitionPlace != "Seleccione una opción" && receptionCampus != "Seleccione una opción" &&
         receptionPlace != "Seleccione una opción" && reason != "Seleccione una opción" && descriptionWhy != "") {
-            var toJsonData = {
-                "Username": "Yulian Adolfo Rojas Gañan",
-                "AssetName": assetName,
-                "Brand": brand,
-                "Model": modal,
-                "Serial": serial,
-                "OtherItems": otherItemsAdded,
-                "EmCampus": emitionCampus,
-                "EmPlace": emitionPlace,
-                "ReCampus": receptionCampus,
-                "RePlace": receptionPlace,
-                "Reason": reason,
-                "Description": descriptionWhy
-            }
-            prepareDataToSend = JSON.stringify(toJsonData)
-        }else {
-            prepareDataToSend = null
-            createAlertToUser("empty", "Aún faltan campos por llenar en esta sección, por favor verifique")
+        var toJsonData = {
+            "Username": "Yulian Adolfo Rojas Gañan",
+            "AssetName": assetName,
+            "Brand": brand,
+            "Model": modal,
+            "Serial": serial,
+            "OtherItems": otherItemsAdded,
+            "EmCampus": emitionCampus,
+            "EmPlace": emitionPlace,
+            "ReCampus": receptionCampus,
+            "RePlace": receptionPlace,
+            "Reason": reason,
+            "Description": descriptionWhy
         }
-        return prepareDataToSend
+        prepareDataToSend = JSON.stringify(toJsonData)
+    } else {
+        prepareDataToSend = null
+        //createAlertToUser("empty", "Aún faltan campos por llenar en esta sección, por favor verifique")
+    }
+    return prepareDataToSend
 }
 sendRequestButton.onclick = (e) => {
     e.preventDefault()
@@ -222,38 +223,36 @@ sendRequestButton.onclick = (e) => {
     if (dataAsset != null) {
         console.log(dataAsset)
         sendingData(dataAsset)
-    }else {
-        console.log("Faltan datos")
+    } else {
+        notificationApp("Existen campos vacios, verifique", "empty")
     }
     async function sendingData(contentRequest) {
         onprogressFunction(panel_window())
-        activateOverFlowApp(false)
-        var stateRequest = await sendRequestToServer(contentRequest)
+        await sendRequestToServer(contentRequest)
         removeLastElementApp()
-        activateOverFlowApp(true)
-        createAlertToUser("successfull", stateRequest.State)
+        notificationApp("Exitoso", "successfull")
     }
     function sendRequestToServer(contentRequest) {
         var stateRequest = new Promise((resolve, reject) => {
             fetch(ADDRESS_SERVER + "new-request-asset", {
-                method:"POST",
+                method: "POST",
                 header: {
                     'content-type': 'application/json'
                 },
                 body: contentRequest
             })
-            .then(response => response.json())
-            .then(data => resolve(data))
-            .catch((error)=> {reject(error)})
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch((error) => { reject(error) })
         })
         return stateRequest
     }
 }
 function getReasons() {
-    dataFromAreasCampus("3s", selectReasonChange)   
+    dataFromAreasCampus("3s", selectReasonChange)
 }
 function appendReasons(dataInJson, selectElement) {
-    for(var i =0; i < dataInJson.length; i++) {
+    for (var i = 0; i < dataInJson.length; i++) {
         var data = JSON.parse(dataInJson[i])
         var opt = document.createElement("option")
         opt.innerHTML = data.Reason_change
@@ -280,12 +279,12 @@ function onprogressFunction(onprogressWin) {
     progressIcon.style.backgroundImage = "url(../public/Images/loading.gif)"
     progressIcon.style.backgroundSize = "contain"
     progressIcon.style.width = "45px"
-    progressIcon.style.height ="45px"
+    progressIcon.style.height = "45px"
     progressIcon.style.display = "block"
     progressIcon.style.margin = "0 auto"
     progressIcon.style.marginTop = "10px"
 
-    progressPanel.style.width  = "300px"
+    progressPanel.style.width = "300px"
     progressPanel.style.height = "100px"
     progressPanel.style.borderRadius = "10px"
     progressPanel.style.backgroundColor = "rgb(30, 219, 5)"
@@ -304,25 +303,25 @@ function createAlertToUser(warning, content) {
     alert.style.height = "100px"
     alert.style.position = "absolute"
     alert.style.right = "20px"
-    alert.style.bottom= "10px"
+    alert.style.bottom = "10px"
     alert.style.borderRadius = "10px"
     alert.style.color = "white"
     alert.style.fontSize = "18px"
     alert.style.display = "flex"
     alert.style.justifyContent = "center"
     alert.style.alignItems = "center"
-    alert.style.textAlign ="center"
+    alert.style.textAlign = "center"
     alert.style.paddingLeft = "8px"
     alert.style.paddingRight = "8px"
     alert.style.transform = "translateX(110%)"
- 
+
     iconX.classList.add("fas", "fa-times")
     iconX.style.position = "absolute"
-    iconX.style.right ="6px"
+    iconX.style.right = "6px"
     iconX.style.top = "5px"
     iconX.style.color = "white"
     iconX.style.fontSize = "15px"
-    switch(warning){
+    switch (warning) {
         case "ERROR":
             color = "red"
             break;
@@ -345,12 +344,12 @@ function createAlertToUser(warning, content) {
         alert.style.transform = "translateX(0%)"
         setTimeout(() => {
             alert.style.transition = ".8s"
-            alert.style.transform = "translateX(110%)" 
+            alert.style.transform = "translateX(110%)"
             setTimeout(() => {
                 document.body.removeChild(alert)
-            }, 1000);
+            }, 3000);
         }, 3000);
-    }, 200);
+    }, 300);
 }
 function retrievingDataProgress() {
     var rtvData = panel_window()
@@ -368,6 +367,49 @@ function retrievingDataProgress() {
     h3.style.fontSize = "15px"
     rtvData.appendChild(h3)
 
+}
+function notificationApp(messageA=undefined, state) {
+    scrollingToTop()
+    // structure of the notification
+    let color = null
+
+    if (state == "successfull") {
+        color = "rgb(3, 223, 58)"
+    }else if (state == "error") {
+        color = "red"
+    }else {
+        color = "rgb(253, 135, 0)"
+    }
+    console.log(color)
+    console.log(state)
+    var containerNotify = document.createElement("div")
+    var message = document.createElement("p")
+    var icon = document.createElement("img")
+    icon.src = "../public/Images/successfull.png"
+    icon.style.width = "25px"
+    icon.style.height = "25px"
+    icon.style.marginRight = "5px"
+    message.innerHTML = messageA
+    containerNotify.style.backgroundColor = color
+    containerNotify.appendChild(icon)
+    containerNotify.appendChild(message)
+    containerNotify.classList.add("container-notification")
+    document.body.appendChild(containerNotify)
+
+    
+    setTimeout(() => {
+        containerNotify.classList.add("container-notification-animation")
+        setTimeout(() => {
+           containerNotify.classList.remove("container-notification-animation") 
+           containerNotify.classList.add("container-notification-animation-up")
+           setTimeout(() => {
+            removeLastElementApp()
+           }, 500);
+        }, 1600);
+    }, 320);
+}
+function scrollingToTop(){
+    window.scroll({top:0, left:0, behavior:"smooth"})
 }
 function activateOverFlowApp(state) {
     state === true ? document.body.style.overflowY = "scroll" : document.body.style.overflowY = "hidden"
